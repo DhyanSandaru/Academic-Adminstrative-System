59af5e4baf207c275a193fefc71403f61a099d49
Senukaaa <senukanandipa@gmail.com>
2025-07-21 18:41:17 +0530

    Add files via upload

---
+++ src/pages/AddLecturer.jsx
@@ -0,0 +1,11 @@
+import LecturerForm from "../components/LecturerForm";
+import { useState } from "react";
+import Layout from "../components/Layout";
+
+export default function AddLecturer() {
+    return(
+        <Layout>
+            <LecturerForm />
+        </Layout>
+    )
+}
---
+++ src/pages/ViewLecturers.jsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+import SearchBar from "../components/Searchbar";
+export default function ViewLecturers(){
+    return(
+        <Layout>
+            <SearchBar />
+            
+        </Layout>
+    )
+}
